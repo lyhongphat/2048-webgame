02f725d263eaadcb396385f7a319e55837a46ca5
khowfsix <lyhongphat261202@gmail.com>
2024-11-17 23:41:05 +0700

    Add new tile in board and render board

--- 2048.js
+++ 2048.js
@@ -9,11 +9,36 @@ class Game2048 {
 
 	initGame() {
 		this.score = 0;
+		this.addRandomTile();
+		this.addRandomTile();
 		this.renderBoard();
 	}
 
+	setupEventListeners() {}
+
 	renderBoard() {
+		this.gameBoard.innerHTML = "";
+		this.tiles.forEach((tile) => {
+			const tileElement = document.createElement("div");
+			tileElement.classList.add("tile");
+			if (tile !== null) {
+				tileElement.classList.add(`tile-${tile}`);
+				tileElement.textContent = tile;
+			}
+			this.gameBoard.appendChild(tileElement);
+		});
 		this.scoreDisplay.textContent = `Score: ${this.score}`;
+	}
+
+	addRandomTile() {
+		const emptyTiles = this.tiles
+			.map((tile, index) => (tile === null ? index : null))
+			.filter((index) => index !== null);
+
+		if (emptyTiles.length === 0) return;
+		const randomIndex =
+			emptyTiles[Math.floor(Math.random() * emptyTiles.length)];
+		this.tiles[randomIndex] = Math.random() < 0.9 ? 2 : 4;
 	}
 }
 
